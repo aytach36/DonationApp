ffac2c0d19bdb39c8f980f393464f28400176106
aytach36 <aytchkf@gmail.com>
2024-01-13 00:37:39 +0200

    Creted Header for Home Page

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {View, SafeAreaView, Text} from 'react-native';
 
+import Header from '../../components/Header/Header';
+
 import globalStyle from '../../assets/styles/globalStyle';
 import style from './style';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <View>
-        <Text>Hello world</Text>
-      </View>
+      <Header title={'Azzahri A.'} type={1} />
+      <Header title={'Azzahri A.'} type={2} />
+      <Header title={'Azzahri A.'} type={3} />
     </SafeAreaView>
   );
 };
