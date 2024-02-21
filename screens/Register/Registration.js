4059b2e89fcee5034a8708d2beb905f9d2b844c5
aytach36 <aytchkf@gmail.com>
2024-02-21 23:35:56 +0200

    Completed Login Screen and Register Screen

--- screens/Register/Registration.js
+++ screens/Register/Registration.js
@@ -8,18 +8,32 @@ import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 
 import {Routes} from '../../navigation/Routes';
+import BackButton from '../../components/BackButton/BackButton';
 
-const Login = ({navigation}) => {
+const Registration = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <View style={style.backButton}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </View>
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={style.container}>
         <View style={globalStyle.marginBottom24}>
-          <Header type={1} title={'Welcome Back'} />
+          <Header type={1} title={'Hello and Welcome'} />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            label={'First & Last name'}
+            placeholder={'Enter your fullname...'}
+            onChangeText={value => setFullName(value)}
+            keyboardType={'email-address'}
+            xzxzz
+          />
         </View>
         <View style={globalStyle.marginBottom24}>
           <Input
@@ -39,16 +53,11 @@ const Login = ({navigation}) => {
           />
         </View>
         <View style={globalStyle.marginBottom24}>
-          <Button title={'Login'} />
+          <Button title={'Register'} />
         </View>
-        <Pressable
-          style={style.registrationButton}
-          onPress={() => navigation.navigate(Routes.Registration)}>
-          <Header color={'#156cF7'} type={3} title={"Don't have an account?"} />
-        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default Login;
+export default Registration;
